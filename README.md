fd9d4e88741875c3c13eb2893ad5789c8a479494
Kaya M <kaya.abiola@gmail.com>
2024-03-12 16:04:14 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# 08-weather-dashboard
+# Unit 08: Weather-Dashboard
 AS A traveler I WANT to see the weather outlook for multiple cities SO THAT I can plan a trip accordingly
+
+
+## Description
+
+Server APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+
+Use the [5 Day Weather Forecast](https://openweathermap.org/forecast5) to retrieve weather data for cities. The link should take you to a guide on how to use the 5 Day Forecast API. You will need to register for an API key in order to use this API. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate.
+
+The base URL for your API calls should look like the following: `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`.
+
+**Hint**: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
+
+You will use `localStorage` to store any persistent data. For more information on how to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys).
+
+## User Story
+
+```text
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+```
+
+## Technology Used
+
+* HTML.
+* JavaScript.
+* CSS.
+* Bootstrap.
+* OpenWeather API (for data).
+
+
+## Mock-Up
+
+The following image shows the web application's appearance and functionality:
+
+![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for London.]
+
+![alt text](<assets/images/weather-dashbord  screenshot.png>)
+
+
+[text](assets/images/Weather-dashboard-screenrecording.mov)
+
+## References
+
+W3 Schools - Tutorials
+
+Youtube - Tutorials
+
+OpenWeathermap:
+ - https://openweathermap.org/forecast5
+
+MDN:
+* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+
+* https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+## License
+
+Licensed under the MIT License
+
+## Repository's
+
+GitHub Repository URL: https://github.com/KayaM17/08-weather-dashboard
+
+Deployed Web Application URL: 
---
+++ assets/js/script.js
@@ -0,0 +1,119 @@
+// Create a weather dashboard with form inputs.
+// When a user searches for a city, they are presented with current and future conditions for that city.
+//Users city search is added to the search history.
+//When a user views the current weather conditions for that city, they are presented with the following:
+// City name.
+// Date.
+// Icon representation of weather conditions.
+// Temperature, Humidity and Wind speed.
+// When user view future weather conditions of any city,
+// they are presented with a 5-day forecast that displays:
+// Date & Icon representation of weather conditions.
+// Temperature & Humidity.
+// https://api.openweathermap.org/data/2.5/forecast?lat=51.5072&lon=0.1276&appid=bff363b19166c862e56389c5d311a2c6
+
+
+$(document).ready(function () {
+    // Search history buttons clickable.
+    $("#history").on("click", ".history-btns", function () {
+        var searchValue = $(this).text();
+        getWeather(searchValue);
+    });
+    // Search button feature.  
+    $("#search-form").on("submit", function (event) {
+        event.preventDefault();        
+        console.log("search button clicked");
+        var searchValue = $("#search-input").val().trim();
+        // Early return if searchValue is empty
+        if (searchValue === "") {
+            // Error message in search box and replace placeholder momentarily for 5 seconds
+            $("#search-input").attr("placeholder", "Please enter a valid city.");
+            setTimeout(function () {
+                $("#search-input").attr("placeholder", "Search for a city...");
+            }, 3000);
+            return; // Stop execution if the search is empty
+        }
+        // Capitalize the first letter of each word
+        searchValue = searchValue.split(" ").map(function (word) {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        }).join(" ");
+        // Fetch weather
+        getWeather(searchValue);        
+        // Search history saved to local storage.
+        var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+        searchHistory.push(searchValue);
+        
+        // If search history is more than 6, remove the oldest search
+        if (searchHistory.length > 6) {
+            searchHistory.shift(); // Only 6 are kept
+        }
+        localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+        updateHistoryDisplay();       
+    });
+
+    function updateHistoryDisplay() {
+        var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+        $("#history").empty(); // Clear existing buttons
+        searchHistory.reverse().forEach(function (searchValue) {
+            var htmlHist = `
+                <div class="d-grid gap-5 mt-3 historyBtns">
+                    <button class="btn history-btns" type="button">${searchValue}</button>
+                </div>
+            `;
+            $("#history").append(htmlHist);
+        });
+    } 
+// Displat history 
+    updateHistoryDisplay();  
+});
+// WEATHER API
+var apiKey = "bff363b19166c862e56389c5d311a2c6"
+function getWeather(searchValue) {
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${apiKey}&units=metric`;
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log("CURRENT DAY: ", data);
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+            getFiveDays(lat, lon);
+            var htmlToday = `
+                <div class="card-big">                    
+                    <h3>${data.name} (${new Date(data.dt * 1000).getDate().toString().padStart(2, '0')}/${(new Date(data.dt * 1000).getMonth() + 1).toString().padStart(2, '0')}/${new Date(data.dt * 1000).getFullYear()})</h3>
+                    <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="weather icons">
+                    <p>Temp: ${Math.round(data.main.temp)}°C</p>
+                    <p>Wind: ${data.wind.speed} KPH</p>
+                    <p>Humidity: ${data.main.humidity} %</p>
+                </div>
+            `;
+            $("#today").html(htmlToday);            
+        });
+}
+
+function getFiveDays(lat, lon) {
+    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {    
+            // 5 day forecast
+            $("#forecast").html("");        
+            for (let i = 0; i < data.list.length; i+=8) {
+                let element = data.list[i];
+                console.log("FIVE DAYS: ", element);
+                var htmlStr = `
+                    <div class="card-item">
+                        <h3>${element.dt_txt.split(" ")[0].split('-').reverse().join('/')}</h3>
+                        <img src="https://openweathermap.org/img/w/${element.weather[0].icon}.png" alt="weather icons">
+                        <p>Temp: ${Math.round(element.main.temp)} °C</p>
+                        <p>Wind: ${element.wind.speed} KPH</p>
+                        <p>Humidity: ${element.main.humidity} %</p>
+                    </div>
+                `;
+                $("#forecast").append(htmlStr);
+            }
+        });
+}
